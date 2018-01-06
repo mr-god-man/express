7fc199199bc95888abbf4f71c3d7464d73a2318d
guojiaxing <18846422687@163.com>
2018-01-06 19:03:37 +0800

    twice

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'f1X1oyRdSFdNPCvtWSWsbG3A-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'fQlthzQzCj5bOEkidbckCGx9',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'JNONSQgcz333r6ddR0cAimyd'
+});
 
 var app = express();
+app.use(AV.express())
 
 
 app.get("/",function(req,res){
 	res.end("123")
 })
 
-app.listen(3000);
+app.listen(process.env.LEANCLOUD_APP_PORT);
